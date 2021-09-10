a6d7952294e4e386fc78cbc56b765bc7b3b64ab8
Haydon Lam <lamhaydon@gmail.com>
2021-09-10 18:48:32 +0100

    Made asset public, removed firebase-admin, fixed netlify function

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,10 +1,4 @@
 // workaround for environment variables https://timdeschryver.dev/blog/environment-variables-with-sveltekit
 export const variables = {
 	github_token: import.meta.env.VITE_GITHUB_TOKEN,
-	firebase_bucket_name: import.meta.env.VITE_FIREBASE_BUCKET_NAME as string,
-	firebase_service_account: {
-		client_email: import.meta.env.VITE_FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL as string,
-		project_id: import.meta.env.VITE_FIREBASE_SERVICE_ACCOUNT_PROJECT_ID as string,
-		private_key: import.meta.env.VITE_FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY as string
-	}
 };
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,10 +5,10 @@
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ page, fetch, session, context }) {
-		const aboutMarkdownReq = await fetch('/api/markdown/about.json');
-		const aboutMarkdownRes = await aboutMarkdownReq.json();
+		const aboutMarkdownReq = await fetch('/api/markdown/about.md');
+		const aboutMarkdownRes = await aboutMarkdownReq.text();
 
-		return { props: { markdown: aboutMarkdownRes.data } };
+		return { props: { markdown: aboutMarkdownRes } };
 	}
 </script>
 
