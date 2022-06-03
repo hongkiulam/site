3241c8b6864bbc2d7401418cbeb7f46e254dc1d1
Haydon Lam <lamhaydon@gmail.com>
2022-06-03 11:22:22 +0100

    Fixes to lightbox

--- src/lib/components/BehanceLightbox.svelte
+++ src/lib/components/BehanceLightbox.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
 	import type { InternalBehanceProject } from '$types/behance';
-	import { createEventDispatcher, onMount, afterUpdate } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+	import Loader from '$lib/components/Loader.svelte';
+
 	export let images: InternalBehanceProject['images'];
 	export let imageIndex: number;
 
 	const dispatch = createEventDispatcher();
+	const possibleSizes = [600, 1200, 1400, 1920, 2800];
 
+	let imageLoading = false;
+	let imageBindingConstraint: 'x' | 'y' | '' = '';
 	let imgEl: HTMLImageElement;
+
 	$: imageSet = images[imageIndex];
 	$: hasImage = !!imageSet;
-	let imageBindingConstraint: 'x' | 'y' | '' = '';
-// TODO add loading spinner, set loading=true when imageIndex is reset to undefined
-// set loading false once image has finished loading
+	$: loading = imageLoading || !imageBindingConstraint;
+
+	$: imageIndex && (imageBindingConstraint = '');
+
 	$: checkImageBounds = () => {
 		const containerW = window.innerWidth;
 		const containerH = window.innerHeight;
-		const imageW = imgEl.naturalWidth;
-		const imageH = imgEl.naturalHeight;
-		const portrait = imageW < imageH;
+		const imageW = imgEl?.naturalWidth;
+		const imageH = imgEl?.naturalHeight;
+		const screenPortrait = containerW < containerH;
 
 		let shouldBindX = false;
 		let shouldBindY = false;
@@ -30,7 +37,7 @@
 		}
 		if (!shouldBindX && !shouldBindY) return (imageBindingConstraint = '');
 		if (shouldBindX && shouldBindY) {
-			return (imageBindingConstraint = portrait ? 'y' : 'x');
+			return (imageBindingConstraint = screenPortrait ? 'x' : 'y');
 		}
 		// one of the bindings is true, only need to check for one
 		imageBindingConstraint = shouldBindX ? 'x' : 'y';
@@ -49,36 +56,57 @@
 />
 
 {#if hasImage}
-	<div class="lightbox">
+	<div class="backdrop fullscreen-center-content" />
+	<div class="loader fullscreen-center-content">
+		<Loader />
+	</div>
+	<div
+		class="lightbox fullscreen-center-content"
+		on:click={(e) => {
+			if (e.target === imgEl) return;
+			dispatch('close');
+		}}
+	>
 		<img
 			on:load={checkImageBounds}
 			bind:this={imgEl}
 			class={imageBindingConstraint}
-			srcset="{imageSet[600].srcset} 600w,
-		{imageSet[1200].srcset} 1200w,
-		{imageSet[1400].srcset} 1400w,
-		{imageSet[2800].srcset} 2800w"
-			sizes="{imageSet[600].media_query} 600px,
-		{imageSet[1200].media_query} 1200px,
-		{imageSet[1400].media_query} 1400px,
-		{imageSet[2800].media_query} 2800px"
+			class:loading
+			srcset={possibleSizes
+				.map((size) => (imageSet[size] ? `${imageSet[size].srcset} ${size}w` : ''))
+				.filter((x) => !!x)
+				.join(', ')}
+			sizes={possibleSizes
+				.map((size) => (imageSet[size] ? `${imageSet[size].media_query} ${size}px` : ''))
+				.filter((x) => !!x)
+				.join(', ')}
 			alt="uh oh"
 		/>
 	</div>
 {/if}
 
 <style>
-	.lightbox {
+	.fullscreen-center-content {
 		position: fixed;
-		z-index: 999;
 		width: 100vw;
 		height: 100vh;
-		background: var(--bg-1);
 		top: 0;
 		left: 0;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+	}
+	.backdrop {
+		background-color: var(--bg-1);
+		opacity: 0.5;
+		backdrop-filter: blur(100px);
+		z-index: 997;
+	}
+	.loader {
+		z-index: 998;
+	}
+	.lightbox {
+		z-index: 999;
 	}
 	img.x {
 		width: 100%;
--- src/lib/utils/get-project-images.ts
+++ src/lib/utils/get-project-images.ts
@@ -1,16 +1,20 @@
 import getBehanceStore from './get-behance-store';
+import type { InternalBehanceProject } from '../../types/behance';
 
-export default async (params: { id: number; slug: string }) => {
+export default async (params: {
+	id: number;
+	slug: string;
+}): Promise<InternalBehanceProject['images']> => {
 	const projectPath = params.id + '/' + params.slug;
 	const pageStore = await getBehanceStore(`https://behance.net/gallery/${projectPath}`);
 	const projectModules = pageStore.project.project.modules;
 	const imageModules = projectModules.filter((mod) =>
 		['image', 'media_collection'].includes(mod.type)
 	);
-	const imageUrls: any[] = [];
+	const imageUrls: InternalBehanceProject['images'] = [];
 	const getImageFromBehanceProjectModule = (projMod) => {
 		const pictureSourceArrayAsObject = {};
-		
+
 		projMod.picture.sources.forEach((src) => {
 			pictureSourceArrayAsObject[src.width] = src;
 		});
