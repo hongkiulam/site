b62d57eff4c8d91ab5f6c4b4b4d765cc65ca7930
Haydon Lam <lamhaydon@gmail.com>
2023-02-26 16:15:07 +0000

    Use threlte-spline package

--- src/lib/components/3d/LandingScene.svelte
+++ src/lib/components/3d/LandingScene.svelte
@@ -1,223 +1,217 @@
 <script lang="ts">
-	import { buildGraph } from '$lib/utils/threlte';
-	import { Canvas, OrbitControls, T, useLoader } from '@threlte/core';
-	import { useCursor } from '@threlte/extras';
-	import { degToRad } from 'three/src/math/MathUtils';
-	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
-	import MeshPhongWithCssColor from './MeshPhongWithCSSColor.svelte';
-	import SplineMesh from './SplineMesh.svelte';
-	import RectAreaLight from './RectAreaLight.svelte';
-	import SplineGroupAndMeshChildren from './SplineGroupAndMeshChildren.svelte';
-	import SplineGroup from './SplineGroup.svelte';
-	import { goto } from '$app/navigation';
+  import { goto } from '$app/navigation';
+  import type { ObjectMap } from 'threlte-spline';
+  import { Canvas, OrbitControls, T } from '@threlte/core';
+  import { useCursor } from '@threlte/extras';
+  import { degToRad } from 'three/src/math/MathUtils';
+  import { createEventDispatcher, onDestroy, onMount } from 'svelte';
+  import MeshPhongWithCssColor from './MeshPhongWithCSSColor.svelte';
+  import SplineMesh from './SplineMesh.svelte';
+  import SplineGroupAndMeshChildren from './SplineGroupAndMeshChildren.svelte';
+  import SplineGroup from './SplineGroup.svelte';
 
-	const dispatch = createEventDispatcher();
-	const { onPointerEnter, onPointerLeave } = useCursor();
-	let splineDataLoaded = false;
-	let nodes: ReturnType<typeof buildGraph>['nodes'];
-	let materials: ReturnType<typeof buildGraph>['materials'];
+  const dispatch = createEventDispatcher();
+  const { onPointerEnter, onPointerLeave } = useCursor();
+  let splineDataLoaded = false;
+  let nodes: ObjectMap['nodes'];
+  let materials: ObjectMap['materials'];
 
-	onMount(async () => {
-		const splineloader = await import('@splinetool/loader').then((mod) => mod.default);
-		const loader = useLoader(splineloader, () => new splineloader());
-		loader.load(
-			`https://prod.spline.design/ZHtnxBIvBXpuWgue/scene.splinecode?t=${Date.now()}`,
-			(spline) => {
-				const graph = buildGraph(spline);
-				nodes = graph.nodes;
-				materials = graph.materials;
-				splineDataLoaded = true;
-				dispatch('splinedataloaded');
-			},
-			() => {},
-			(err) => {
-				console.log(err);
-			}
-		);
-	});
+  onMount(async () => {
+    const loadSpline = (await import('threlte-spline')).loadSpline;
+    loadSpline(`https://prod.spline.design/ZHtnxBIvBXpuWgue/scene.splinecode?t=${Date.now()}`)
+      .then((graph) => {
+        nodes = graph.nodes;
+        materials = graph.materials;
+        splineDataLoaded = true;
+        dispatch('splinedataloaded');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-	const isMobile = typeof window !== 'undefined' ? 'ontouchstart' in window : false;
-	type Links = '/photos' | '/code' | '/about';
-	const preObjectActionEventName = 'pre-object-action';
-	let mobileAutoDismissTimeout: number;
-	const hoverOverObject = (href: Links, hovered: boolean) => {
-		// disable on mobile
-		if (isMobile) return;
-		(hovered ? onPointerEnter : onPointerLeave)();
-		dispatch(preObjectActionEventName, hovered ? href : '');
-	};
-	const clickObject = (href: Links) => {
-		if (isMobile) {
-			// prompt the toast to appear on mobile
-			dispatch(preObjectActionEventName, href);
-			window.clearTimeout(mobileAutoDismissTimeout);
-			mobileAutoDismissTimeout = window.setTimeout(() => {
-				dispatch(preObjectActionEventName, '');
-			}, 6000);
-		} else {
-			goto(href);
-		}
-	};
+  const isMobile = typeof window !== 'undefined' ? 'ontouchstart' in window : false;
+  type Links = '/photos' | '/code' | '/about';
+  const preObjectActionEventName = 'pre-object-action';
+  let mobileAutoDismissTimeout: number;
+  const hoverOverObject = (href: Links, hovered: boolean) => {
+    // disable on mobile
+    if (isMobile) return;
+    (hovered ? onPointerEnter : onPointerLeave)();
+    dispatch(preObjectActionEventName, hovered ? href : '');
+  };
+  const clickObject = (href: Links) => {
+    if (isMobile) {
+      // prompt the toast to appear on mobile
+      dispatch(preObjectActionEventName, href);
+      window.clearTimeout(mobileAutoDismissTimeout);
+      mobileAutoDismissTimeout = window.setTimeout(() => {
+        dispatch(preObjectActionEventName, '');
+      }, 6000);
+    } else {
+      goto(href);
+    }
+  };
 
-	onDestroy(() => {
-		if (typeof window !== 'undefined') {
-			window.clearTimeout(mobileAutoDismissTimeout);
-		}
-	});
+  onDestroy(() => {
+    if (typeof window !== 'undefined') {
+      window.clearTimeout(mobileAutoDismissTimeout);
+    }
+  });
 </script>
 
 <!-- Spline uses linear encoding for colours, this sets ctx.renderer.outputEncoding = LinearEncoding so
 that colors and lighting are aligned with what is in spline -->
 <!-- Rationale for flat https://github.com/pmndrs/react-three-fiber/discussions/1290 -->
 <Canvas linear flat>
-	{#if splineDataLoaded}
-		<T.PerspectiveCamera
-			makeDefault
-			position={nodes.sceneCamera.position}
-			fov={nodes.sceneCamera.fov}
-			zoom={window.innerWidth < 900 ? 0.5 : nodes.sceneCamera.zoom}
-		>
-			<!-- min|maxZoom for OrthographicCamera -->
-			<!-- min|maxDistance for PerspectiveCamera -->
-			<OrbitControls
-				maxPolarAngle={degToRad(97)}
-				minPolarAngle={degToRad(70)}
-				minAzimuthAngle={degToRad(-20)}
-				maxAzimuthAngle={degToRad(20)}
-				minDistance={50}
-				maxDistance={175}
-				enableZoom
-				enablePan={true}
-				enableDamping
-				rotateSpeed={0.5}
-			/>
-		</T.PerspectiveCamera>
+  {#if splineDataLoaded}
+    <T.PerspectiveCamera
+      makeDefault
+      position={nodes.sceneCamera.position}
+      fov={nodes.sceneCamera.fov}
+      zoom={window.innerWidth < 900 ? 0.5 : nodes.sceneCamera.zoom}
+    >
+      <!-- min|maxZoom for OrthographicCamera -->
+      <!-- min|maxDistance for PerspectiveCamera -->
+      <OrbitControls
+        maxPolarAngle={degToRad(97)}
+        minPolarAngle={degToRad(70)}
+        minAzimuthAngle={degToRad(-20)}
+        maxAzimuthAngle={degToRad(20)}
+        minDistance={50}
+        maxDistance={175}
+        enableZoom
+        enablePan={true}
+        enableDamping
+        rotateSpeed={0.5}
+      />
+    </T.PerspectiveCamera>
 
-		<T.DirectionalLight
-			castShadow
-			position={nodes['Directional Light'].position}
-			intensity={nodes['Directional Light'].intensity}
-			color={nodes['Directional Light'].color}
-			scale={nodes['Directional Light'].scale}
-			shadow={nodes['Directional Light'].shadow}
-		/>
-		<T.HemisphereLight
-			intensity={nodes['Default Ambient Light'].intensity - 0.2}
-			color={nodes['Default Ambient Light'].color}
-		/>
+    <T.DirectionalLight
+      castShadow
+      position={nodes['Directional Light'].position}
+      intensity={nodes['Directional Light'].intensity}
+      color={nodes['Directional Light'].color}
+      scale={nodes['Directional Light'].scale}
+      shadow={nodes['Directional Light'].shadow}
+    />
+    <T.HemisphereLight
+      intensity={nodes['Default Ambient Light'].intensity - 0.2}
+      color={nodes['Default Ambient Light'].color}
+    />
 
-		<!-- Environment Objects -->
-		<SplineMesh node={nodes['baseWall']}>
-			<MeshPhongWithCssColor color="color-bg-1" />
-		</SplineMesh>
-		<SplineMesh node={nodes['backWall']}>
-			<MeshPhongWithCssColor color="color-bg-1" />
-		</SplineMesh>
+    <!-- Environment Objects -->
+    <SplineMesh node={nodes['baseWall']}>
+      <MeshPhongWithCssColor color="color-bg-1" />
+    </SplineMesh>
+    <SplineMesh node={nodes['backWall']}>
+      <MeshPhongWithCssColor color="color-bg-1" />
+    </SplineMesh>
 
-		<!-- Computer Parts -->
-		<SplineGroup node={nodes['mac']}>
-			{#each nodes['mac'].children as child}
-				{#if child.name !== 'macImage'}
-					<SplineMesh node={child} />
-				{:else}
-					<SplineMesh
-						node={child}
-						interactive
-						on:click={() => clickObject('/code')}
-						on:hover={(e) => hoverOverObject('/code', e.detail)}
-					/>
-				{/if}
-			{/each}
-		</SplineGroup>
-		<SplineGroupAndMeshChildren node={nodes['keyboard']} />
+    <!-- Computer Parts -->
+    <SplineGroup node={nodes['mac']}>
+      {#each nodes['mac'].children as child}
+        {#if child.name !== 'macImage'}
+          <SplineMesh node={child} />
+        {:else}
+          <SplineMesh
+            node={child}
+            interactive
+            on:click={() => clickObject('/code')}
+            on:hover={(e) => hoverOverObject('/code', e.detail)}
+          />
+        {/if}
+      {/each}
+    </SplineGroup>
+    <SplineGroupAndMeshChildren node={nodes['keyboard']} />
 
-		<SplineGroup node={nodes['monitor']}>
-			<SplineMesh node={nodes['monitorFrame']}>
-				<MeshPhongWithCssColor color="color-copy-2" />
-			</SplineMesh>
-			<SplineMesh node={nodes['monitorArm']}>
-				<MeshPhongWithCssColor color="color-copy-2" />
-			</SplineMesh>
-			<SplineMesh node={nodes['monitorScreen']} />
-		</SplineGroup>
+    <SplineGroup node={nodes['monitor']}>
+      <SplineMesh node={nodes['monitorFrame']}>
+        <MeshPhongWithCssColor color="color-copy-2" />
+      </SplineMesh>
+      <SplineMesh node={nodes['monitorArm']}>
+        <MeshPhongWithCssColor color="color-copy-2" />
+      </SplineMesh>
+      <SplineMesh node={nodes['monitorScreen']} />
+    </SplineGroup>
 
-		<SplineGroupAndMeshChildren node={nodes['mouse']} />
+    <SplineGroupAndMeshChildren node={nodes['mouse']} />
 
-		<!-- Picture Objects -->
-		<SplineGroup node={nodes['picture1']}>
-			<SplineMesh
-				node={nodes['canvas1']}
-				interactive
-				on:click={() => clickObject('/photos')}
-				on:hover={(e) => hoverOverObject('/photos', e.detail)}
-			/>
-		</SplineGroup>
+    <!-- Picture Objects -->
+    <SplineGroup node={nodes['picture1']}>
+      <SplineMesh
+        node={nodes['canvas1']}
+        interactive
+        on:click={() => clickObject('/photos')}
+        on:hover={(e) => hoverOverObject('/photos', e.detail)}
+      />
+    </SplineGroup>
 
-		<SplineGroup node={nodes['picture2']}>
-			<SplineMesh
-				node={nodes['canvas2']}
-				interactive
-				on:click={() => clickObject('/photos')}
-				on:hover={(e) => hoverOverObject('/photos', e.detail)}
-			/>
-		</SplineGroup>
+    <SplineGroup node={nodes['picture2']}>
+      <SplineMesh
+        node={nodes['canvas2']}
+        interactive
+        on:click={() => clickObject('/photos')}
+        on:hover={(e) => hoverOverObject('/photos', e.detail)}
+      />
+    </SplineGroup>
 
-		<!-- Camera -->
-		<SplineGroupAndMeshChildren node={nodes['camera']} />
-		<SplineGroupAndMeshChildren node={nodes['lens']} />
-		<SplineGroupAndMeshChildren node={nodes['lens2']} />
-		<SplineGroupAndMeshChildren node={nodes['sdCard']} />
-		<SplineGroupAndMeshChildren node={nodes['sdCard2']} />
-		<SplineGroupAndMeshChildren node={nodes['cameraBatteries']} />
+    <!-- Camera -->
+    <SplineGroupAndMeshChildren node={nodes['camera']} />
+    <SplineGroupAndMeshChildren node={nodes['lens']} />
+    <SplineGroupAndMeshChildren node={nodes['lens2']} />
+    <SplineGroupAndMeshChildren node={nodes['sdCard']} />
+    <SplineGroupAndMeshChildren node={nodes['sdCard2']} />
+    <SplineGroupAndMeshChildren node={nodes['cameraBatteries']} />
 
-		<!-- Journals -->
-		<SplineGroup node={nodes['orangeJournal']}>
-			<SplineMesh
-				node={nodes['orangeJournalBack']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-			<SplineMesh
-				node={nodes['orangeJournalPaper']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-		</SplineGroup>
-		<SplineGroup node={nodes['blackJournal']}>
-			<SplineMesh
-				node={nodes['blackJournalBack']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-			<SplineMesh
-				node={nodes['blackJournalPaper']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-		</SplineGroup>
-		<SplineGroup node={nodes['blueJournal']}>
-			<SplineMesh
-				node={nodes['blueJournalBack']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-			<SplineMesh
-				node={nodes['blueJournalPaper']}
-				interactive
-				on:click={() => clickObject('/about')}
-				on:hover={(e) => hoverOverObject('/about', e.detail)}
-			/>
-		</SplineGroup>
+    <!-- Journals -->
+    <SplineGroup node={nodes['orangeJournal']}>
+      <SplineMesh
+        node={nodes['orangeJournalBack']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+      <SplineMesh
+        node={nodes['orangeJournalPaper']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+    </SplineGroup>
+    <SplineGroup node={nodes['blackJournal']}>
+      <SplineMesh
+        node={nodes['blackJournalBack']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+      <SplineMesh
+        node={nodes['blackJournalPaper']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+    </SplineGroup>
+    <SplineGroup node={nodes['blueJournal']}>
+      <SplineMesh
+        node={nodes['blueJournalBack']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+      <SplineMesh
+        node={nodes['blueJournalPaper']}
+        interactive
+        on:click={() => clickObject('/about')}
+        on:hover={(e) => hoverOverObject('/about', e.detail)}
+      />
+    </SplineGroup>
 
-		<!-- Arrows -->
-		<SplineGroupAndMeshChildren node={nodes['pointerPicture1']} />
-		<SplineGroupAndMeshChildren node={nodes['pointerPicture2']} />
-		<SplineGroupAndMeshChildren node={nodes['pointerMac']} />
-		<SplineGroupAndMeshChildren node={nodes['pointerJournal']} />
-	{/if}
+    <!-- Arrows -->
+    <SplineGroupAndMeshChildren node={nodes['pointerPicture1']} />
+    <SplineGroupAndMeshChildren node={nodes['pointerPicture2']} />
+    <SplineGroupAndMeshChildren node={nodes['pointerMac']} />
+    <SplineGroupAndMeshChildren node={nodes['pointerJournal']} />
+  {/if}
 </Canvas>
--- src/lib/components/3d/Room.svelte
+++ src/lib/components/3d/Room.svelte
@@ -1,170 +1,165 @@
 <script lang="ts">
-	import { buildGraph } from '$lib/utils/threlte';
-	import { Canvas, OrbitControls, T, useLoader } from '@threlte/core';
-	import { degToRad } from 'three/src/math/MathUtils';
-	import { createEventDispatcher, onMount } from 'svelte';
-	import MeshPhongWithCssColor from './MeshPhongWithCSSColor.svelte';
-	import SplineMesh from './SplineMesh.svelte';
-	import RectAreaLight from './RectAreaLight.svelte';
-	import SplineGroupAndMeshChildren from './SplineGroupAndMeshChildren.svelte';
-	import SplineGroup from './SplineGroup.svelte';
+  import { Canvas, OrbitControls, T, useLoader } from '@threlte/core';
+  import { degToRad } from 'three/src/math/MathUtils';
+  import { createEventDispatcher, onMount } from 'svelte';
+  import MeshPhongWithCssColor from './MeshPhongWithCSSColor.svelte';
+  import SplineMesh from './SplineMesh.svelte';
+  import RectAreaLight from './RectAreaLight.svelte';
+  import SplineGroupAndMeshChildren from './SplineGroupAndMeshChildren.svelte';
+  import SplineGroup from './SplineGroup.svelte';
+  import type { ObjectMap } from 'threlte-spline';
 
-	const dispatch = createEventDispatcher();
-	let splineDataLoaded = false;
-	let nodes: ReturnType<typeof buildGraph>['nodes'];
-	let materials: ReturnType<typeof buildGraph>['materials'];
+  const dispatch = createEventDispatcher();
+  let splineDataLoaded = false;
+  let nodes: ObjectMap['nodes'];
+  let materials: ObjectMap['materials'];
 
-	onMount(async () => {
-		const splineloader = await import('@splinetool/loader').then((mod) => mod.default);
-		const loader = useLoader(splineloader, () => new splineloader());
-		loader.load(
-			`https://prod.spline.design/ZAS6Jqf5PXr-57mU/scene.splinecode?t=${Date.now()}`,
-			(spline) => {
-				const graph = buildGraph(spline);
-				nodes = graph.nodes;
-				materials = graph.materials;
-				splineDataLoaded = true;
-				dispatch('splinedataloaded');
-			},
-			() => {},
-			(err) => {
-				console.log(err);
-			}
-		);
-	});
+  onMount(async () => {
+    const loadSpline = (await import('threlte-spline')).loadSpline;
+    loadSpline(`https://prod.spline.design/ZAS6Jqf5PXr-57mU/scene.splinecode?t=${Date.now()}`)
+      .then((graph) => {
+        nodes = graph.nodes;
+        materials = graph.materials;
+        splineDataLoaded = true;
+        dispatch('splinedataloaded');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-	// useCursor for setting pointer - perhaps just change the colour of things being hovered
+  // useCursor for setting pointer - perhaps just change the colour of things being hovered
 
-	const zoomLevelToViewportRatio = 1 / 900; // zoom / pixels
-	const sensibleMaxSceneWidth = 1300;
-	const minZoom = 0.5;
+  const zoomLevelToViewportRatio = 1 / 900; // zoom / pixels
+  const sensibleMaxSceneWidth = 1300;
+  const minZoom = 0.5;
 </script>
 
 <!-- Spline uses linear encoding for colours, this sets ctx.renderer.outputEncoding = LinearEncoding so
 that colors and lighting are aligned with what is in spline -->
 <!-- Rationale for flat https://github.com/pmndrs/react-three-fiber/discussions/1290 -->
 <Canvas linear flat>
-	{#if splineDataLoaded}
-		<T.OrthographicCamera
-			makeDefault
-			position={nodes.sceneCamera.position}
-			fov={nodes.sceneCamera.fov}
-			zoom={Math.max(
-				minZoom,
-				Math.min(window.innerWidth, sensibleMaxSceneWidth) * zoomLevelToViewportRatio
-			)}
-		>
-			<!-- min|maxZoom for OrthographicCamera -->
-			<!-- min|maxDistance for PerspectiveCamera -->
-			<OrbitControls
-				maxPolarAngle={degToRad(90)}
-				minPolarAngle={degToRad(30)}
-				minAzimuthAngle={degToRad(10)}
-				maxAzimuthAngle={degToRad(80)}
-				{minZoom}
-				maxZoom={6}
-				minDistance={500}
-				maxDistance={1750}
-				enableZoom
-				enablePan={true}
-				enableDamping
-				rotateSpeed={0.5}
-			/>
-		</T.OrthographicCamera>
+  {#if splineDataLoaded}
+    <T.OrthographicCamera
+      makeDefault
+      position={nodes.sceneCamera.position}
+      fov={nodes.sceneCamera.fov}
+      zoom={Math.max(
+        minZoom,
+        Math.min(window.innerWidth, sensibleMaxSceneWidth) * zoomLevelToViewportRatio
+      )}
+    >
+      <!-- min|maxZoom for OrthographicCamera -->
+      <!-- min|maxDistance for PerspectiveCamera -->
+      <OrbitControls
+        maxPolarAngle={degToRad(90)}
+        minPolarAngle={degToRad(30)}
+        minAzimuthAngle={degToRad(10)}
+        maxAzimuthAngle={degToRad(80)}
+        {minZoom}
+        maxZoom={6}
+        minDistance={500}
+        maxDistance={1750}
+        enableZoom
+        enablePan={true}
+        enableDamping
+        rotateSpeed={0.5}
+      />
+    </T.OrthographicCamera>
 
-		<T.DirectionalLight
-			castShadow
-			position={nodes['Directional Light'].position}
-			intensity={nodes['Directional Light'].intensity}
-			color={nodes['Directional Light'].color}
-			scale={nodes['Directional Light'].scale}
-			shadow={nodes['Directional Light'].shadow}
-		/>
-		<T.HemisphereLight
-			intensity={nodes['Default Ambient Light'].intensity - 0.2}
-			color={nodes['Default Ambient Light'].color}
-		/>
+    <T.DirectionalLight
+      castShadow
+      position={nodes['Directional Light'].position}
+      intensity={nodes['Directional Light'].intensity}
+      color={nodes['Directional Light'].color}
+      scale={nodes['Directional Light'].scale}
+      shadow={nodes['Directional Light'].shadow}
+    />
+    <T.HemisphereLight
+      intensity={nodes['Default Ambient Light'].intensity - 0.2}
+      color={nodes['Default Ambient Light'].color}
+    />
 
-		<!-- Environment Objects -->
-		<SplineMesh node={nodes['wall']}>
-			<MeshPhongWithCssColor color="color-bg-1" />
-		</SplineMesh>
-		<SplineMesh node={nodes['carpet']}>
-			<MeshPhongWithCssColor color="color-bg-2" />
-		</SplineMesh>
+    <!-- Environment Objects -->
+    <SplineMesh node={nodes['wall']}>
+      <MeshPhongWithCssColor color="color-bg-1" />
+    </SplineMesh>
+    <SplineMesh node={nodes['carpet']}>
+      <MeshPhongWithCssColor color="color-bg-2" />
+    </SplineMesh>
 
-		<RectAreaLight node={nodes['windowLightPanelPlaceholder1']} color="white" />
-		<RectAreaLight node={nodes['windowLightPanelPlaceholder2']} color="white" />
-		<SplineMesh node={nodes['blinds1']} />
-		<SplineMesh node={nodes['blinds2']} />
+    <RectAreaLight node={nodes['windowLightPanelPlaceholder1']} color="white" />
+    <RectAreaLight node={nodes['windowLightPanelPlaceholder2']} color="white" />
+    <SplineMesh node={nodes['blinds1']} />
+    <SplineMesh node={nodes['blinds2']} />
 
-		<!-- Work Station Objects -->
-		<SplineGroup node={nodes['tableParts']} >
-			<SplineMesh node={nodes['tableTop']} />
-			<SplineMesh node={nodes['leg']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh>
-			<SplineMesh node={nodes['leg1']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh
-			><SplineMesh node={nodes['leg2']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh
-			><SplineMesh node={nodes['leg3']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh>
-		</SplineGroup>
-		<SplineGroupAndMeshChildren node={nodes['officeChair']} />
-		<SplineGroupAndMeshChildren node={nodes['mac']} />
-		<SplineGroupAndMeshChildren node={nodes['keyboard']} />
+    <!-- Work Station Objects -->
+    <SplineGroup node={nodes['tableParts']}>
+      <SplineMesh node={nodes['tableTop']} />
+      <SplineMesh node={nodes['leg']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh>
+      <SplineMesh node={nodes['leg1']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh
+      ><SplineMesh node={nodes['leg2']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh
+      ><SplineMesh node={nodes['leg3']}><MeshPhongWithCssColor color="color-copy-2" /></SplineMesh>
+    </SplineGroup>
+    <SplineGroupAndMeshChildren node={nodes['officeChair']} />
+    <SplineGroupAndMeshChildren node={nodes['mac']} />
+    <SplineGroupAndMeshChildren node={nodes['keyboard']} />
 
-		<SplineGroup node={nodes['monitor']} >
-			<SplineMesh node={nodes['monitorFrame']}>
-				<MeshPhongWithCssColor color="color-copy-2" />
-			</SplineMesh>
-			<SplineMesh node={nodes['monitorArm']}>
-				<MeshPhongWithCssColor color="color-copy-2" />
-			</SplineMesh>
-			<RectAreaLight node={nodes['monitorScreen']} color="#e2fcff" />
-		</SplineGroup>
+    <SplineGroup node={nodes['monitor']}>
+      <SplineMesh node={nodes['monitorFrame']}>
+        <MeshPhongWithCssColor color="color-copy-2" />
+      </SplineMesh>
+      <SplineMesh node={nodes['monitorArm']}>
+        <MeshPhongWithCssColor color="color-copy-2" />
+      </SplineMesh>
+      <RectAreaLight node={nodes['monitorScreen']} color="#e2fcff" />
+    </SplineGroup>
 
-		<SplineGroupAndMeshChildren node={nodes['mouse']} />
-		<SplineMesh node={nodes['bin']}>
-			<MeshPhongWithCssColor color="color-bg-2" /></SplineMesh
-		>
+    <SplineGroupAndMeshChildren node={nodes['mouse']} />
+    <SplineMesh node={nodes['bin']}>
+      <MeshPhongWithCssColor color="color-bg-2" /></SplineMesh
+    >
 
-		<!-- Picture Objects -->
-		<SplineGroup node={nodes['picture1']} >
-			<SplineMesh node={nodes['pictureFrame1']}>
-				<MeshPhongWithCssColor color="color-bg-2" />
-			</SplineMesh>
-			<SplineMesh node={nodes['canvas1']} />
-		</SplineGroup>
+    <!-- Picture Objects -->
+    <SplineGroup node={nodes['picture1']}>
+      <SplineMesh node={nodes['pictureFrame1']}>
+        <MeshPhongWithCssColor color="color-bg-2" />
+      </SplineMesh>
+      <SplineMesh node={nodes['canvas1']} />
+    </SplineGroup>
 
-		<SplineGroup node={nodes['picture2']} >
-			<SplineMesh node={nodes['pictureFrame2']}>
-				<MeshPhongWithCssColor color="color-bg-2" />
-			</SplineMesh>
-			<SplineMesh node={nodes['canvas2']} />
-		</SplineGroup>
+    <SplineGroup node={nodes['picture2']}>
+      <SplineMesh node={nodes['pictureFrame2']}>
+        <MeshPhongWithCssColor color="color-bg-2" />
+      </SplineMesh>
+      <SplineMesh node={nodes['canvas2']} />
+    </SplineGroup>
 
-		<!-- Exercise Area -->
-		<SplineMesh node={nodes['exerciseMat']} />
-		<SplineGroupAndMeshChildren node={nodes['dumbbell1']} />
-		<SplineGroupAndMeshChildren node={nodes['dumbbell2']} />
-		<!-- <SplineGroupAndMeshChildren node={nodes['gym_bag']} /> -->
+    <!-- Exercise Area -->
+    <SplineMesh node={nodes['exerciseMat']} />
+    <SplineGroupAndMeshChildren node={nodes['dumbbell1']} />
+    <SplineGroupAndMeshChildren node={nodes['dumbbell2']} />
+    <!-- <SplineGroupAndMeshChildren node={nodes['gym_bag']} /> -->
 
-		<!-- Shelves -->
-		<SplineMesh node={nodes['shelf1']}>
-			<MeshPhongWithCssColor color="color-copy-2" />
-		</SplineMesh>
-		<SplineMesh node={nodes['shelf2']}>
-			<MeshPhongWithCssColor color="color-copy-2" />
-		</SplineMesh>
+    <!-- Shelves -->
+    <SplineMesh node={nodes['shelf1']}>
+      <MeshPhongWithCssColor color="color-copy-2" />
+    </SplineMesh>
+    <SplineMesh node={nodes['shelf2']}>
+      <MeshPhongWithCssColor color="color-copy-2" />
+    </SplineMesh>
 
-		<!-- Camera -->
-		<SplineGroupAndMeshChildren node={nodes['camera']} />
-		<SplineGroupAndMeshChildren node={nodes['lens']} />
-		<SplineGroupAndMeshChildren node={nodes['lens2']} />
-		<SplineGroupAndMeshChildren node={nodes['sdCard']} />
-		<SplineGroupAndMeshChildren node={nodes['sdCard2']} />
-		<SplineGroupAndMeshChildren node={nodes['cameraBatteries']} />
+    <!-- Camera -->
+    <SplineGroupAndMeshChildren node={nodes['camera']} />
+    <SplineGroupAndMeshChildren node={nodes['lens']} />
+    <SplineGroupAndMeshChildren node={nodes['lens2']} />
+    <SplineGroupAndMeshChildren node={nodes['sdCard']} />
+    <SplineGroupAndMeshChildren node={nodes['sdCard2']} />
+    <SplineGroupAndMeshChildren node={nodes['cameraBatteries']} />
 
-		<!-- Journals -->
-		<SplineGroupAndMeshChildren  node={nodes['orangeJournal']}/>
-		<SplineGroupAndMeshChildren  node={nodes['blueJournal']}/>
-		<SplineGroupAndMeshChildren  node={nodes['blackJournal']}/>
-	{/if}
+    <!-- Journals -->
+    <SplineGroupAndMeshChildren node={nodes['orangeJournal']} />
+    <SplineGroupAndMeshChildren node={nodes['blueJournal']} />
+    <SplineGroupAndMeshChildren node={nodes['blackJournal']} />
+  {/if}
 </Canvas>
