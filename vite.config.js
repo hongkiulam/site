2079ef2b235859e50523af32e209ee5ac2baa14e
Haydon Lam <lamhaydon@gmail.com>
2023-05-27 21:08:21 +0100

    Upgrade threlte spline

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { threlteSplineTypesPlugin } from 'threlte-spline/dist/types-plugin';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	ssr: {
-		noExternal: ['three', 'throija-three-text']
-	}
+  plugins: [sveltekit(), threlteSplineTypesPlugin()],
+  ssr: {
+    noExternal: ['three', 'throija-three-text']
+  }
 };
 
 export default config;
