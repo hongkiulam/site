9edf264762e42ee2b7b63b84470c307981dfd8a3
Haydon Lam <lamhaydon@gmail.com>
2025-03-22 16:12:35 +0000

    add tailwind and sst

---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "site-v2",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Astro("MyWeb");
+  },
+});
