b096e115e4fad6763c4d2d7ad4d582d0798f0429
Haydon Lam <lamhaydon@gmail.com>
2025-05-10 10:57:26 +0100

    increase nav hit area

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
 import S from "./MobileNav.module.css";
-import { animate } from "animejs";
 import MobileNavPageWipe from "./MobileNavPageWipe";
-import { Menu, X, ChevronLeft } from "lucide-react";
+import { X, ChevronLeft } from "lucide-react";
 
 const MobileNav = () => {
   const [open, setOpen] = useState(false);
@@ -12,85 +11,83 @@ const MobileNav = () => {
     if (!dialogRef.current) return;
     dialogRef.current.showModal();
     setOpen(true);
-    animate(dialogRef.current, {
-      // opacity: 1,
-      // duration: 200,
-      // delay: 450,
-      // x: { to: "-100%", duration: 0 },
-    });
   };
 
   const hideModal = () => {
     if (!dialogRef.current) return;
     dialogRef.current.close();
     setOpen(false);
-    // animate(dialogRef.current, {
-    //   duration: 200,
-    //   opacity: 0,
-    //   x: { to: "0%", duration: 0, delay: 500 },
-    // });
   };
 
   return (
     <>
-      <button
-        onClick={showModal}
-        aria-label="Open Navigation"
-        className="-mr-2 cursor-pointer"
-      >
-        <ChevronLeft />
-      </button>
-      <dialog
-        ref={dialogRef}
-        onClose={hideModal}
-        className={`${S["mobile-nav"]} w-full min-w-full left-full min-h-screen bg-transparent h-screen fixed top-0 text-muted-background
+      <menu className="flex items-end flex-col justify-center">
+        <button
+          tabIndex={-1}
+          className="text-muted-foreground text-sm cursor-pointer"
+          onClick={showModal}
+        >
+          Open Navigation
+        </button>
+        <button
+          onClick={showModal}
+          aria-label="Open Navigation"
+          className="-mr-2 cursor-pointer pl-20"
+        >
+          <ChevronLeft />
+        </button>
+        <dialog
+          ref={dialogRef}
+          onClose={hideModal}
+          className={`${S["mobile-nav"]} w-full min-w-full left-full min-h-screen bg-transparent h-screen fixed top-0 text-muted-background
         `}
-      >
-        <form className="h-[90px] flex flex-col justify-center items-start px-12 py-4 max-w-[1440px] mx-auto max-sm:px-6">
-          <div className="flex items-start flex-col justify-center">
-            <small
-              className="text-muted-background text-sm"
-              id="close-navigation-label"
-            >
-              Close Navigation
-            </small>
-            <menu>
+        >
+          <form className="h-[90px] flex flex-col justify-center items-start px-12 py-4 max-w-[1440px] mx-auto max-sm:px-6">
+            <menu className="flex items-start flex-col justify-center">
+              <button
+                onClick={hideModal}
+                className="text-muted-background text-sm cursor-pointer"
+                tabIndex={-1}
+                id="close-navigation-label"
+              >
+                Close Navigation
+              </button>
               <button
                 autoFocus
                 formMethod="dialog"
                 aria-labelledby="close-navigation-label"
-                className="-ml-1 cursor-pointer"
+                className="-ml-1 cursor-pointer pr-20"
               >
                 <X />
               </button>
             </menu>
+          </form>
+          <div className="clamped">
+            <ul className="flex flex-col gap-3">
+              <li>
+                <h2>
+                  <a href="/">Home</a>
+                </h2>
+              </li>
+              <li>
+                <h2>Projects</h2>
+              </li>
+              <li>
+                <h2>
+                  <a href="/photos">Photography</a>
+                </h2>
+              </li>
+              <li>
+                <h2>Lab/ Experiments</h2>
+              </li>
+              <li>
+                <h2>Spoon Carving</h2>
+              </li>
+            </ul>
           </div>
-        </form>
-        <div className="clamped">
-          <ul className="flex flex-col gap-3">
-            <li>
-              <h2>
-                <a href="/">Home</a>
-              </h2>
-            </li>
-            <li>
-              <h2>Projects</h2>
-            </li>
-            <li>
-              <h2>
-                <a href="/photos">Photography</a>
-              </h2>
-            </li>
-            <li>
-              <h2>Lab/ Experiments</h2>
-            </li>
-            <li>
-              <h2>Spoon Carving</h2>
-            </li>
-          </ul>
-        </div>
-      </dialog>
-      <MobileNavPageWipe isOpen={open} />
+        </dialog>
+        <MobileNavPageWipe isOpen={open} />
+      </menu>
     </>
   );
 };
